cdc8b30eeb41046f47e2ee868208bb386b7fdb14
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-08-23 09:32:12 -0400

    me falta ingresar los links

--- prueba.rb
+++ prueba.rb
@@ -15,9 +15,7 @@ datos = JSON.parse response.read_body
 puts datos.class
 
 
-def build_web_page(datos)
-
-url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=eEn9DjQUdeT9RQZez6td0qaNSjaOSgDtPDLys1Wj")
+url = URI("https://api.nasa.gov/planetary/apod?api_key=h48Vr2hydyLaFVb3CQguyIOSDFxoRKrs27wlD4OT")
 
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
@@ -35,10 +33,23 @@ archivo_salida.puts "<html>
         </head>
     <body>
         <ul>"
+def build_web_page(datos)
+    datos.each do |foto|
+        url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=#{foto["photos"]["img_src"]}&api_key=eEn9DjQUdeT9RQZez6td0qaNSjaOSgDtPDLys1Wj")
+
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+
+        request = Net::HTTP::Get.new(url)
+
+        response = https.request(request)
+        img = JSON.parse(response.read_body)
+
+        archivo_salida.puts "#{foto["photos"]["img_src"]}"
+    end
+end
 archivo_salida.puts "</ul>
 </body>
 </html>"
 archivo_salida.close
-end
-
 build_web_page(datos)
