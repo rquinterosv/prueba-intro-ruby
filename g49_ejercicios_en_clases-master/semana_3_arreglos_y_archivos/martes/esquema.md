2efcf4e6985b3d4c8bec2938c1aa77936afa0648
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-08-24 20:52:44 -0400

    Entrega de prueba

--- g49_ejercicios_en_clases-master/semana_3_arreglos_y_archivos/martes/esquema.md
+++
@@ -1,4 +0,0 @@
-0. Arreglos con while I
-1. Arreglos con each_with_index e I
-2. Arreglos con each_with_index y arreglo no mas
-3. Arreglos con metodo .max
--- prueba.rb
+++ prueba.rb
@@ -2,7 +2,7 @@ require "uri"
 require "net/http"
 require "json"
 
-url = URI("https://api.nasa.gov/planetary/apod?api_key=h48Vr2hydyLaFVb3CQguyIOSDFxoRKrs27wlD4OT")
+url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=eEn9DjQUdeT9RQZez6td0qaNSjaOSgDtPDLys1Wj")
 
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
@@ -10,22 +10,10 @@ https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 
 response = https.request(request)
-
-datos = JSON.parse response.read_body
-puts datos.class
+datos = JSON.parse(response.read_body)
 
 
-url = URI("https://api.nasa.gov/planetary/apod?api_key=h48Vr2hydyLaFVb3CQguyIOSDFxoRKrs27wlD4OT")
-
-https = Net::HTTP.new(url.host, url.port)
-https.use_ssl = true
-
-request = Net::HTTP::Get.new(url)
-
-response = https.request(request)
-
-datos = JSON.parse response.read_body
-
+def build_web_page(datos)
 archivo_salida = File.open("prueba.html", "w")
 archivo_salida.puts "<html>
         <head>
@@ -33,23 +21,27 @@ archivo_salida.puts "<html>
         </head>
     <body>
         <ul>"
-def build_web_page(datos)
-    datos.each do |foto|
-        url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=#{foto["photos"]["img_src"]}&api_key=eEn9DjQUdeT9RQZez6td0qaNSjaOSgDtPDLys1Wj")
 
-        https = Net::HTTP.new(url.host, url.port)
-        https.use_ssl = true
+        datos["photos"].each do |foto|
+            archivo_salida.puts "<li><img src=""#{foto["img_src"]}""></li>"
+        end        
 
-        request = Net::HTTP::Get.new(url)
-
-        response = https.request(request)
-        img = JSON.parse(response.read_body)
-
-        archivo_salida.puts "#{foto["photos"]["img_src"]}"
-    end
-end
 archivo_salida.puts "</ul>
 </body>
 </html>"
 archivo_salida.close
+    end
+
+def photos_count(datos)
+    archivo_fotos = File.open("photos.html", "w")
+    archivo_fotos.puts "Nombre Camara"
+
+    camaras = datos["photos"]
+    
+    camaras.each do |cam|
+        archivo_fotos.puts "#{cam["camera"]["full_name"]}"
+    end
+    archivo_fotos.close
+end
 build_web_page(datos)
+photos_count(datos)
